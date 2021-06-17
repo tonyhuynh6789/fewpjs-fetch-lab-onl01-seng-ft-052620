52d76a5e0c9a1e66bb2437b0b3997e9e00d02d07
tonyhuynh6789 <duc_thinh_03@yahoo.com>
2021-06-17 15:06:50 -0700

    commit tree 24519f7cf240e6602d6c5697814d0a3ad2d76bf1

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-
 function fetchBooks() {
   return fetch('https://anapioficeandfire.com/api/books')
   .then(resp => resp.json())
-  .then(json => renderBooks(json));
+  .then (json => renderBooks(json))
 }
 
+
 function renderBooks(books) {
-  const main = document.querySelector('main')
+    const main = document.querySelector('main')
   books.forEach(book => {
     const h2 = document.createElement('h2')
-    h2.innerHTML = book.name
+    h2.innerText = book.name 
     main.appendChild(h2)
-  })
+  });
 }
+
+
 
 
 
